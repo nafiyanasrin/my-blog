8425c16996542ce8e63325d58dba6958144da384
nafiyanasrin <165750555+nafiyanasrin@users.noreply.github.com>
2024-04-18 23:14:44 -0700

    Add files via upload

---
+++ frontend/src/components/blog/blog.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import './blog.css'  
+function Myblog() {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+   
+    useEffect(()=>{
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => { setData(res.data);
+      setLoading(false);
+    })
+      .catch(err => { console.log(err);
+      setLoading(false);
+      });
+
+    },[])
+
+    if (loading) {
+        return <div>Loading</div>;
+      }
+  return (
+    <div className='container'>
+    
+<table  className='table'>
+<thead>
+
+    <tr>
+        <th>userID</th>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Body</th>      
+  </tr>
+
+</thead>
+
+<tbody>
+
+{
+      data.map((post, index) => {
+        return <tr key={index}>
+          <td>{post.userId}</td>
+          <td>{post.id}</td>
+          <td>{post.title}</td>
+          <td>{post.body}</td>
+
+
+
+
+        </tr>
+
+      })
+
+}
+
+</tbody>
+
+
+</table>
+
+
+
+
+
+
+
+
+    
+    </div>
+  )
+}
+export default Myblog;
